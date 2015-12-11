6c8354b9f237ff1703995c0d2e5c98818074ae87
Jeffrey Leung <jeffrey.leung@cloverhealth.com>
2015-12-11 11:20:36 -0800

    revised selinux priority

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,9 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 include_recipe 'observium::set_repo'
+include_recipe 'observium::selinux_permissive'
 include_recipe 'observium::observium'
 include_recipe 'observium::database'
-include_recipe 'observium::selinux_permissive'
 include_recipe 'observium::config_observium'
 include_recipe 'observium::observium_cron'
 # include_recipe 'observium::firewall_permithttp'
